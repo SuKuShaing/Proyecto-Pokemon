abc187f8d6375b84d692087bbcb7c467b2fdfec6
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-17 18:15:57 -0400

    Como pasarle un componente a una isla

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,18 @@
-import { createSignal, type Component } from "solid-js";
+import { createSignal, type Component, type JSX } from "solid-js";
 
 interface Props {
-	initValue: number;	
+	initValue: number;
+	children?: JSX.Element; // para que el componente pueda recibir hijos
 }
 
-export const Counter: Component<Props> = (props) => {
+export const Counter: Component<Props> = (props) => { // sí desestructuramos propr, pierde la reactividad (pasa solo en solid)
 
     const [ counter, setCounter ] = createSignal(props.initValue);
 
 	return (
 		<>
-			<h1 class="text-4xl">Counter</h1>
+			{/* <h1 class="text-4xl">Counter</h1> */}
+			{props.children}
 			<h3 class="text-xl">Value: { counter() } </h3>
 			<button 
                 onClick={ () => setCounter(counter() + 1) }
