54da4cf44d4b46b001a16213733bc36f47a6aa7a
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-20 00:23:43 -0400

    Añadir componente FavoritePokemonCard y actualizar FavoritePokemons para mostrar o ocultar los Pokémon favoritos.

---
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -0,0 +1,47 @@
+import type { FavoritePokemon } from "@/interfaces/favorite-pokemon.response";
+import type { JSX } from "solid-js";
+import { Show, createSignal, type Component } from "solid-js";
+
+interface Props {
+	pokemon: FavoritePokemon;
+}
+
+export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
+
+    const [ isVisible, setIsVisible ] = createSignal(true);
+
+	const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
+    const deleteFavorite = () => {
+        const favorites = JSON.parse(
+            localStorage.getItem("favorites") ?? "[]"
+        ) as FavoritePokemon[];
+
+        const newFavorites = favorites.filter(p => p.id !== pokemon.id);
+
+        localStorage.setItem('favorites', JSON.stringify(newFavorites));
+        setIsVisible(false);
+    }
+
+	return (
+        <Show when={isVisible()} >
+            <div class="flex flex-col justify-center items-center">
+                <a
+                    href={`/pokemons/${pokemon.name}`}
+                    class="bg-slate-700 border-2 border-slate-700 rounded-lg shadow-md p-3 group hover:border-slate-100 transition-border duration-400"
+                    >
+                    <img
+                        // transition:name={`${pokemon.name}-image`}
+                        class="transition-transform duration-400 group-hover:scale-110"
+                        src={imageSrc}
+                        alt={pokemon.name}
+                        />
+                    <h2 class="text-center text-2xl font-semibold text-slate-200 capitalize">
+                        {pokemon.name}
+                    </h2>
+                </a>
+                <button onClick={deleteFavorite} class="text-red-400 cursor-pointer">Borrar</button>
+            </div>
+        </Show>
+	);
+};
--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,5 +1,6 @@
 import type { FavoritePokemon } from "@/interfaces/favorite-pokemon.response";
 import { For, createSignal } from "solid-js";
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
 
 const getLocalStoragePokemons = (): FavoritePokemon[] => {
 	const favoritePokemons = JSON.parse(
@@ -14,7 +15,9 @@ export const FavoritePokemons = () => {
 
 	return (
 		<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-6">
-			<For each={pokemons()}>{(pokemon) => <h1>{pokemon.name}</h1>}</For>
+			<For each={pokemons()}>
+				{(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
+			</For>
 		</div>
 	);
 };
